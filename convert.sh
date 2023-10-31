5361cb1a69da24599ba6680135f12458393fa40e
Aaron Daughtry <aarondaughtry@gmail.com>
2023-10-31 18:29:57 -0400

    Added commands for creating a virtual environment, if one is not present, and activating it, as well as installing requirements.

--- convert.sh
+++ convert.sh
@@ -7,4 +7,10 @@ for filename in $input/{normal,italic}/*.{woff,woff2}; do
 	./woff2/woff2_decompress $filename
 done
 
+if [ ! -d "./.venv" ] && [ ! -d "./venv" ]; then
+	python3 -m venv .venv
+fi
+
+source ./.venv/bin/activate
+python3 -m pip install -r requirements.txt
 python3 to_static.py
